c1fb1a7caee43b6c23f28f96099d12f6ad380db0
lmve <1831232566@qq.com>
2025-01-04 23:49:39 +0800

    some exercises.

--- exercises/04_static/main.cpp
+++ exercises/04_static/main.cpp
@@ -2,18 +2,30 @@
 
 // READ: `static` 关键字 <https://zh.cppreference.com/w/cpp/language/storage_duration>
 // THINK: 这个函数的两个 `static` 各自的作用是什么？
-static int func(int param) {
-    static int static_ = param;
-    // std::cout << "static_ = " << static_ << std::endl;
+static int func(int param) {  // 1
+    static int static_ = param; // 2
+    std::cout << "static_ = " << static_ << std::endl;
     return static_++;
 }
 
+/*
+ * 1. 函数定义前，表示该函数具有静态链接属性，
+ *    函数 func 只能在定义它的文件中调用而不能被其他文件中的代码所调用，
+ *    有助于避免命名冲突和减少编译后的符号表大小
+
+ * 2. 变量前表示这个变量具有静态存储期 
+ *    被 static 关键字描述的变量在第一次被初始化后，
+ *    其值会在函数的调用之间保持不变，直到程序结束
+
+ * static 在这里分别用于控制函数的作用域和变量的生命周期
+*/
+
 int main(int argc, char **argv) {
     // TODO: 将下列 `?` 替换为正确的数字
-    ASSERT(func(5) == ?, "static variable value incorrect");
-    ASSERT(func(4) == ?, "static variable value incorrect");
-    ASSERT(func(3) == ?, "static variable value incorrect");
-    ASSERT(func(2) == ?, "static variable value incorrect");
-    ASSERT(func(1) == ?, "static variable value incorrect");
+    ASSERT(func(5) == 5, "static variable value incorrect");
+    ASSERT(func(4) == 6, "static variable value incorrect");
+    ASSERT(func(3) == 7, "static variable value incorrect");
+    ASSERT(func(2) == 8, "static variable value incorrect");
+    ASSERT(func(1) == 9, "static variable value incorrect");
     return 0;
 }
--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -1,6 +1,6 @@
 #include "../exercise.h"
 
-constexpr unsigned long long fibonacci(int i) {
+unsigned long long fibonacci(auto i) {
     switch (i) {
         case 0:
             return 0;
@@ -12,15 +12,30 @@ constexpr unsigned long long fibonacci(int i) {
 }
 
 int main(int argc, char **argv) {
-    constexpr auto FIB20 = fibonacci(20);
+    auto FIB20 = fibonacci(20);
     ASSERT(FIB20 == 6765, "fibonacci(20) should be 6765");
     std::cout << "fibonacci(20) = " << FIB20 << std::endl;
 
     // TODO: 观察错误信息，修改一处，使代码编译运行
     // PS: 编译运行，但是不一定能算出结果……
-    constexpr auto ANS_N = 90;
-    constexpr auto ANS = fibonacci(ANS_N);
+    auto ANS_N = 90;
+    auto ANS = fibonacci(ANS_N);
     std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
     return 0;
 }
+
+
+/*
+ * 1. constexpr 修饰的函数时函数体尽量只包含一个 return 语句，包含多个可能会触发编译错误
+      函数体可以包含其他语句，但是不能是运行期的语句，
+      编译器会将 constexpr 函数视为内联函数，在编译时若能求出其值，则会把函数调用替换成结果值
+   2. 在类的构造函数中使用 constexpr 关键字，保证传递给该构造函数的所有参数都是 constexpr, 
+      产生的对象的所有成员都是 constexpr, 该对象只可用于 constexpr 的场合。
+      cosntexpr 的函数体必须为空，所有的成员变量的初始化都放到初始化列表中。 
+
+ * * cosnt 与 constexpr 的区别
+   1. const 变量的初始化可以延迟到运行时，而 cosntexpr 变量必须在编译时进行初始化，
+      所有 constexor 变量均为常量，因此必须使用常量表达式初始化
+   2. 在 constexpr 声明中如果定义了指针，限定符仅对指针有效， 与指针所指对象无关
+*/
